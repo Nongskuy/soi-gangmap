f7c05cfcc484ee17887a7e375a820fa4988e2988
Nongskuy <vinog4ming@gmail.com>
2023-03-26 21:33:16 +0800

    remove something

--- js/app.js
+++ js/app.js
@@ -102,12 +102,6 @@ $(function() {
 
 	var categories = (window.cats = new CategoriesCollection([
 		{
-			name: 'Neighborhoods',
-			icon: 'radar/radar_warehouse.png',
-			type: 'General',
-			enabled: false,
-		},
-		{
 			name: 'Territories',
 			icon: 'General/wall-breach.png',
 			type: 'General',
@@ -117,13 +111,13 @@ $(function() {
 			name: 'Neutral',
 			icon: 'General/glitches.png',
 			type: 'General',
-			enabled: true,
+			enabled: false,
 		},
 		{
 			name: 'Automotive',
 			icon: 'radar/radar_acsr_race_hotring.png',
 			type: 'General',
-			enabled: true,
+			enabled: false,
 		},
 		{
 			name: 'Legal',
@@ -135,13 +129,13 @@ $(function() {
 			name: 'Medical',
 			icon: 'radar/radar_hospital.png',
 			type: 'General',
-			enabled: true,
+			enabled: false,
 		},
 		{
 			name: 'Restaurants',
 			icon: 'radar/radar_bar.png',
 			type: 'General',
-			enabled: true,
+			enabled: false,
 		},
 		/*
 		{
