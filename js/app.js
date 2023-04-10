cc1622a31294c9f75d74944e12224087005e8b07
Nongskuy <vinog4ming@gmail.com>
2023-04-10 21:50:47 +0800

    -Add For The Lost MC -Remove Restaurant, Automotive, Neutral, etc. -make this map for illegal and police map

--- js/app.js
+++ js/app.js
@@ -108,35 +108,11 @@ $(function() {
 			enabled: true,
 		},
 		{
-			name: 'Neutral',
-			icon: 'General/glitches.png',
-			type: 'General',
-			enabled: false,
-		},
-		{
-			name: 'Automotive',
-			icon: 'radar/radar_acsr_race_hotring.png',
-			type: 'General',
-			enabled: false,
-		},
-		{
 			name: 'Legal',
 			icon: 'radar/radar_police_station.png',
 			type: 'General',
 			enabled: true,
 	   },
-		{
-			name: 'Medical',
-			icon: 'radar/radar_hospital.png',
-			type: 'General',
-			enabled: false,
-		},
-		{
-			name: 'Restaurants',
-			icon: 'radar/radar_bar.png',
-			type: 'General',
-			enabled: false,
-		},
 		/*
 		{
 			name: 'Deprecated',
